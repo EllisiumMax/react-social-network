34ed844a86ab3022d8813a53d124f4cd6ad28d4e
EllisiumMax <sailorjerry777@gmail.com>
2021-01-26 20:03:20 +0200

    created basic UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.scss';
+import toucanLogo from './images/toucan.svg';
+
 
 function App() {
   return (
-<h1>Hello world</h1>
+    <div className="app-wrapper">
+<header className="header">
+  <img src={toucanLogo}></img>
+   <p>Toucan Social Network</p>
+</header>
+<nav className="nav">
+    <a>Profile</a>
+    <a>Messages</a>
+    <a>News</a>
+    <a>Settings</a>
+</nav>
+<main className="content">
+MAIN CONTENT
+</main>
+</div>
   );
 }
 
