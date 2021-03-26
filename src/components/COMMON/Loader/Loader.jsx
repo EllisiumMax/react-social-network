b584a1b9db00786a0eebf9150f5f9e6ace2064d8
EllisiumMax <sailorjerry777@gmail.com>
2021-03-26 19:30:00 +0200

    ProfileContainer changed from class to func component, fixed bug when profile page didnt load own profile

--- src/components/COMMON/Loader/Loader.jsx
+++ src/components/COMMON/Loader/Loader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import loader from "../../../images/loader.svg";
 import scss from "./Loader.module.scss";
 
-function Loader({fullscreen}) {
+function Loader({fullscreen = false}) {
   return (
 <div className={scss.loaderWrapper}>
       <img className={fullscreen ? scss.fullScreenLoader : scss.loader} src={loader} alt="LOADING..." />
--- src/components/profile/ProfileContainer.jsx
+++ src/components/profile/ProfileContainer.jsx
@@ -1,6 +1,6 @@
 import Loader from "components/COMMON/Loader/Loader";
 import withAuthRedirect from "hoc/withAuthRedirect";
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { compose } from "redux";
@@ -19,15 +19,14 @@ import {
 } from "redux/profileSelectors";
 import Profile from "./Profile";
 
-class ProfileContainer extends React.Component {
-  componentDidMount() {
-    this.props.loadProfile(this.props.match.params.userId ?? this.props.id);
-    this.props.statusRequest(this.props.match.params.userId ?? this.props.id);
-  }
 
-  render() {
-    return this.props.isFetching ? <Loader /> : <Profile {...this.props} />;
-  }
+function ProfileContainer (props) {
+  useEffect( () => {
+    props.loadProfile(props.match.params.userId ?? props.id);
+    props.statusRequest(props.match.params.userId ?? props.id);
+  },[props.location.key])
+
+  return props.isFetching ? <Loader /> : <Profile {...props} />;
 }
 
 function mapStateToProps(state) {
@@ -38,6 +37,8 @@ function mapStateToProps(state) {
     isFetching: getFetchingSel(state),
   };
 }
+
+
 export default compose(
   connect(mapStateToProps, {
     loadProfile,
