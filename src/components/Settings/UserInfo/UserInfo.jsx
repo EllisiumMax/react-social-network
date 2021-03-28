63afa0055b22f77e2d194f82fc3f0e061ef82b71
EllisiumMax <sailorjerry777@gmail.com>
2021-03-28 12:54:15 +0300

    Settings page design improved

--- src/components/Settings/UserInfo/UserInfo.jsx
+++ src/components/Settings/UserInfo/UserInfo.jsx
@@ -4,7 +4,7 @@ import { Field, Form } from "react-final-form";
 import scss from "./UserInfo.module.scss";
 
 function UserInfo(props) {
-  if(!props.profileInfo.fullName) return <Loader />;
+  if (!props.profileInfo.fullName) return <Loader />;
   return (
     <div>
       <Form
@@ -16,118 +16,138 @@ function UserInfo(props) {
         render={({ handleSubmit, submitting }) => (
           <form onSubmit={handleSubmit}>
             <div className={scss.mainWrapper}>
-              <div>
-                <label>Full name: </label>
-                <Field
-                  name="fullName"
-                  component="input"
-                  type="text"
-                  defaultValue={props.profileInfo.fullName}
-                  
-                />
+              <div className={`${scss.sectionContainer} ${scss.personalInfo}`}>
+                <h4 className={scss.sectionHeader}>Personal Info</h4>
+                <div>
+                  <label>Full name: </label>
+                  <Field
+                    name="fullName"
+                    component="input"
+                    type="text"
+                    defaultValue={props.profileInfo.fullName}
+                  />
+                </div>
+                <div>
+                  <label>About me: </label>
+                  <Field
+                    name="aboutMe"
+                    component="input"
+                    type="text"
+                    defaultValue={props.profileInfo.aboutMe}
+                  />
+                </div>
               </div>
-              <div>
-                <label>About me: </label>
-                <Field name="aboutMe" component="input" type="text" defaultValue={props.profileInfo.aboutMe}/>
-              </div>
-              <div>
-                <label>Looking for a job?</label>
-                <Field
-                  name="lookingForAJob"
-                  component="input"
-                  type="checkbox"
-                  checked={props.profileInfo.lookingForAJob ? "checked" : null}
-                />
-              </div>
-              <div>
-                <label>Describe the job you are looking for: </label>
-                <Field
-                  name="lookingForAJobDescription"
-                  component="input"
-                  type="text"
-                  defaultValue={props.profileInfo.lookingForAJobDescription}
-                />
+              <div className={`${scss.sectionContainer} ${scss.jobInfo}`}>
+                <h4 className={scss.sectionHeader}>Job Information</h4>
+                <div>
+                  <label>Looking for a job?</label>
+                  <Field
+                    name="lookingForAJob"
+                    component="input"
+                    type="checkbox"
+                    checked={
+                      props.profileInfo.lookingForAJob ? "checked" : null
+                    }
+                  />
+                </div>
+                <div className={scss.job}>
+                  <label>Describe the job you are looking for: </label>
+                  <Field
+                    name="lookingForAJobDescription"
+                    component="input"
+                    type="text"
+                    defaultValue={props.profileInfo.lookingForAJobDescription}
+                  />
+                </div>
               </div>
 
-              <div className={scss.contactsWrapper}>
-                <label>Contacts</label>
-                <label>
-                  GitHub:
+              <div className={`${scss.sectionContainer} ${scss.contactsInfo}`}>
+                <h4 className={scss.sectionHeader}>Contacts</h4>
+                <div>
+                  <label>GitHub:</label>
                   <Field
                     name="contacts.github"
                     component="input"
                     type="text"
                     defaultValue={props.profileInfo.contacts.github}
                   />
-                </label>
-                <label>
-                  Vkontakte:
+                </div>
+                <div>
+                  <label>Vkontakte:</label>
                   <Field
                     name="contacts.vk"
                     component="input"
                     type="text"
                     defaultValue={props.profileInfo.contacts.vk}
-
                   />
-                </label>
-                <label>
-                  Facebook:
+                </div>
+
+                <div>
+                  <label>Facebook:</label>
                   <Field
                     name="contacts.facebook"
                     component="input"
                     type="text"
                     defaultValue={props.profileInfo.contacts.facebook}
                   />
-                </label>
-                <label>
-                  Instagram:
+                </div>
+
+                <div>
+                  <label>Instagram:</label>
                   <Field
                     name="contacts.instagram"
                     component="input"
                     type="text"
                     defaultValue={props.profileInfo.contacts.instagram}
                   />
-                </label>
-                <label>
-                  Twitter:
+                </div>
+
+                <div>
+                  <label>Twitter:</label>
                   <Field
                     name="contacts.twitter"
                     component="input"
                     type="text"
                     defaultValue={props.profileInfo.contacts.twitter}
                   />
-                </label>
-                <label>
-                  Website:
+                </div>
+
+                <div>
+                  <label>Website:</label>
                   <Field
                     name="contacts.website"
                     component="input"
                     type="text"
                     defaultValue={props.profileInfo.contacts.website}
                   />
-                </label>
-                <label>
-                  Youtube:
+                </div>
+
+                <div>
+                  <label>Youtube:</label>
                   <Field
                     name="contacts.youtube"
                     component="input"
                     type="text"
                     defaultValue={props.profileInfo.contacts.youtube}
                   />
-                </label>
-                <label>
-                  Main Link:
+                </div>
+                <div>
+                  <label>Main Link:</label>
                   <Field
                     name="contacts.mainLink"
                     component="input"
                     type="text"
                     defaultValue={props.profileInfo.contacts.mainLink}
                   />
-                </label>
-                <button type="submit" disabled={submitting} className={scss.submit}>
-                  Submit
-                </button>
+                </div>
               </div>
+              <button
+                type="submit"
+                disabled={submitting}
+                className={scss.submit}
+              >
+                Submit
+              </button>
             </div>
           </form>
         )}
